cb627235eb2d60c4e472ee4a18b53a8bc41e93c0
Jerry Liu <ljliu89@gmail.com>
2025-04-28 16:50:49 -0700

    feat: add chat interface

---
+++ src/components/ChatInterFace.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import avatar from "../assets/avatar.jpg";
+
+export const ChatInterFace: React.FC = () => {
+  return (
+    <div className="m-4">
+      <div className="relative">
+        <button
+          className="bg-[url(./assets/back-btn.svg)] bg-transparent bg-no-repeat bg-center
+           w-6 h-6 border-none absolute left-0"
+        ></button>
+        <div className="flex flex-col items-center justify-center gap-2 m-auto">
+          <img src={avatar} alt="avatar" className="w-9 h-9 rounded-full" />
+          <p className="font-medium text-base leading-[110%] tracking-[0%] ">
+            Mary Jane
+          </p>
+        </div>
+      </div>
+      <form className="relative w-[304px] h-[42px]">
+        <input
+          type="text"
+          placeholder="Write a message..."
+          className="border border-gray-300 p-3 rounded-[12px] w-full
+          font-normal text-base leading-[110%]"
+        />
+        <button
+          className="bg-[url(./assets/subtract-btn.svg)] bg-transparent bg-no-repeat bg-center
+           w-6 h-6 border-none absolute top-2 right-3"
+        ></button>
+      </form>
+    </div>
+  );
+};
---
+++ src/components/Main.tsx
@@ -0,0 +1,15 @@
+//TODO: replace with real main component
+
+import React  from "react";
+import { ChatInterFace } from "./ChatInterFace";
+
+export const Main: React.FC = ()=>{
+
+    return(
+        <div>
+            <h1 className="text-center">CommuniTEE</h1>
+            <ChatInterFace />
+
+        </div>
+    )
+}
