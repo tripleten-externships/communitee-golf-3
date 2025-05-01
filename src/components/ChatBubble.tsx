e36dbe9365c545850aa4efd0062e555fc6e465b9
Jerry Liu <ljliu89@gmail.com>
2025-04-30 22:12:45 -0700

    fix: fix message time display

--- src/components/ChatBubble.tsx
+++ src/components/ChatBubble.tsx
@@ -2,18 +2,50 @@ import React from "react";
 
 type ChatBubbleProps = {
   isMine?: boolean;
-  message?: string;
+  message: string;
+  sentAt: string;
 };
 
-export const ChatBubble: React.FC<ChatBubbleProps> = ({ isMine, message }) => {
+function getRelativeTime(sentAt: string) {
+  const now = new Date();
+  const timeSent = new Date(sentAt);
+
+  const minDiff = Math.floor((now.getTime() - timeSent.getTime()) / 60000);
+
+  if (minDiff < 60) {
+    return `${minDiff}min`;
+  } else {
+    const hours = Math.floor(minDiff / 60);
+    return `${hours}h`;
+  }
+}
+
+export const ChatBubble: React.FC<ChatBubbleProps> = ({
+  isMine,
+  message,
+  sentAt,
+}) => {
   //   const [isMine, setIsMine] = React.useState(false);
+  const timeDisplay = getRelativeTime(sentAt);
+
   return (
-    <div
-      className={`p-3 max-w-[260px] w-fit h-15 
-        rounded-tr-lg rounded-br-lg rounded-bl-lg gap-2.5
-        ${isMine ? "bg-[#FFDFDF] ml-auto" : "bg-[#DEDEDE4D] mr-auto"}`}
-    >
-      <p>{message}</p>
+    <div className="flex flex-col">
+      <span
+        className={`text-gray-400 text-[10px] leading-[130%] tracking-[0] align-middle 
+            ${isMine ? "self-end" : " self-start"}`}
+      >
+        {timeDisplay}
+      </span>
+      <div
+        className={`p-3 max-w-[260px] w-fit h-15 rounded-lg gap-2.5
+        ${
+          isMine
+            ? "bg-[#FFDFDF] ml-auto rounded-tr-none"
+            : "bg-[#DEDEDE4D] mr-auto rounded-tl-none"
+        }`}
+      >
+        <p>{message}</p>
+      </div>
     </div>
   );
 };
--- src/components/ChatStream.tsx
+++ src/components/ChatStream.tsx
@@ -4,8 +4,21 @@ import { ChatBubble } from "./ChatBubble";
 export const ChatStream: React.FC = () => {
   return (
     <div className="m-1 flex flex-col gap-1">
-      <ChatBubble isMine={false} message="Hey!" />
-      <ChatBubble isMine={true} message="How are you!" />
+      <ChatBubble
+        isMine={false}
+        message="Hey!"
+        sentAt={new Date(Date.now() - 55 * 60 * 1000).toISOString()}
+      />
+      <ChatBubble
+        isMine={true}
+        message="How are you!"
+        sentAt={new Date(Date.now() - 30 * 60 * 1000).toISOString()}
+      />
+      <ChatBubble
+        isMine={false}
+        message="How are you! Nice to meet you all!"
+        sentAt={new Date(Date.now() - 20 * 60 * 1000).toISOString()}
+      />
     </div>
   );
 };
