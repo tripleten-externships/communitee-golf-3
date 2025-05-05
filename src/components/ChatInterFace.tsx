1eb79de4a8646bd99203d6c0d963333a690196e5
Jerry Liu <ljliu89@gmail.com>
2025-05-04 21:37:46 -0700

    feat: add ChatHeader component

--- src/components/ChatInterFace.tsx
+++ src/components/ChatInterFace.tsx
@@ -1,22 +1,11 @@
 import React from "react";
-import avatar from "../assets/avatar.jpg";
 import { ChatStream } from "./ChatStream";
+import { ChatHeader } from "./ChatHeader";
 
 export const ChatInterFace: React.FC = () => {
   return (
     <>
-      <div className="relative">
-        <button
-          className="bg-[url(./assets/back-btn.svg)] bg-transparent bg-no-repeat bg-center
-           w-6 h-6 border-none absolute left-0"
-        ></button>
-        <div className="flex flex-col items-center justify-center gap-2 m-auto">
-          <img src={avatar} alt="avatar" className="w-9 h-9 rounded-full" />
-          <p className="font-medium text-base leading-[110%] tracking-[0%] ">
-            Mary Jane
-          </p>
-        </div>
-      </div>
+      <ChatHeader />
       <ChatStream />
     </>
   );
