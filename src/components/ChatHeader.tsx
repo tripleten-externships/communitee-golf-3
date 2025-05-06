0f597954db950d3728d1ed81fd23a07799902f73
Jerry Liu <ljliu89@gmail.com>
2025-05-05 22:01:34 -0700

    fix: fix avatar cursor pointer

--- src/components/ChatHeader.tsx
+++ src/components/ChatHeader.tsx
@@ -10,7 +10,11 @@ export const ChatHeader: React.FC = () => {
            w-6 h-6 border-none absolute left-0"
       ></button>
       <div className="flex flex-col items-center justify-center gap-2 m-auto">
-        <img src={avatar} alt="avatar" className="w-9 h-9 rounded-full" />
+        <img
+          src={avatar}
+          alt="avatar"
+          className="w-9 h-9 rounded-full cursor-pointer"
+        />
         <p className="font-medium text-base leading-[110%] tracking-[0%] ">
           Mary Jane
         </p>
