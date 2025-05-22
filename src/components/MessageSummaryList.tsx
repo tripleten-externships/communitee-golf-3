578c7f97a805cae5177c6fc626cc55b7156d5b3b
TaeJoo Kim <taejook1107@gmail.com>
2025-05-22 18:22:08 -0400

    feat: made Tabber into a separate component.

--- src/components/MessageSummaryList.tsx
+++ src/components/MessageSummaryList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { MessageSummary } from "./MessageSummary";
+import { Tabber } from "./Tabber";
 
 export interface MessageStream {
   id: string;
@@ -66,16 +67,7 @@ export const MessageSummaryList: React.FC<MessageSummaryListProps> = ({
 
   return (
     <div className="w-full flex flex-col items-center ">
-      <div className="w-full flex flex-col items-center gap-4 mb-4 px-2 border-b-2 border-gray-300 pb-2">
-        <div className="flex items-center gap-2">
-          <button 
-        onClick={() => window.location.href = '/message-stream'}
-        className="text-[16px] font-poppins font-medium hover:text-blue-500 transition-colors cursor-pointer "
-      >
-        Messages ({totalUnreadMessages})
-      </button>
-        </div>
-      </div>
+      <Tabber totalUnreadMessages={totalUnreadMessages} />
       <ul className="flex flex-col gap-[12px] p-0 m-0 max-h-full overflow-y-auto">
         {sortedMessages.map((message) => (
           <li key={message.id}>
