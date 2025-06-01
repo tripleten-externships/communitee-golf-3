574aa753a4bf74ad336b6f77c2ae5d94e38d133e
Jerry Liu <ljliu89@gmail.com>
2025-06-01 00:20:21 -0700

    fix: tie display

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,13 +1,13 @@
 //TODO: replace with real main component
 
-import React from "react";
-import { ChatInterFace } from "./ChatInterFace";
+// import React from "react";
+// import { ChatInterFace } from "./ChatInterFace";
 
-export const Main: React.FC = () => {
-  return (
-    <>
-      <h1 className="text-center">CommuniTEE</h1>
-      <ChatInterFace />
-    </>
-  );
-};
+// export const Main: React.FC = () => {
+//   return (
+//     <>
+//       <h1 className="text-center">CommuniTEE</h1>
+//       <ChatInterFace />
+//     </>
+//   );
+// };
